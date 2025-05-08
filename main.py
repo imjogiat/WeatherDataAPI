0fe7cd8923e835bc32a97299d64f6fdd216f05db
Ismail <imjogiat@gmail.com>
2025-05-08 07:31:57 -0600

    First steps/code in creating Weather Data API

---
+++ main.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 30
+
+    #return a view of the json file data
+    return{"station:": station, 
+           "date:": date,
+           "temperature:": temperature}
+    
+    
+if __name__ == "__main__":
+    app.run(debug=True)
